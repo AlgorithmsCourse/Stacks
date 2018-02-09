f5f2f204582bede9d74f5db59fe5773d39c99de3
Beth Hart <errordrivendev@gmail.com>
2018-02-08 19:43:44 -0800

    add simple interactive console program to build fixedsize stack

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,7 +6,45 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's build a stack of strings!");
+            Console.WriteLine("Enter a stack you would like to build: FixedSize or ResizingArray");
+            var type = Console.ReadLine().ToLower();
+            if (type == "fixedsize")
+            {
+                Console.WriteLine("what size array?");
+                var size = Console.ReadLine();
+                var s = new Stack_FixedSize(Int32.Parse(size));
+
+                var action = "start";
+                while (action != "q")
+                {
+                    Console.WriteLine("To add a word type 'add' or to remove the top item from the stack type 'remove'");
+                    action = Console.ReadLine();
+                    if (action == "add")
+                    {
+                        Console.WriteLine("Enter a word to add to the stack");
+                        var newWord = Console.ReadLine();
+                        s.Push(newWord);
+                        Console.WriteLine($"{newWord} was added to the stack. Your stack now has {s.Size()} items.");
+                        
+                    }
+                    else if (action == "remove")
+                    {
+                        Console.WriteLine($"{s.Pop()} has been removed from the stack. Your stack now has {s.Size()} items.");
+                    } 
+                    else if (action == "q")
+                    {
+                        break;
+                    }
+                }
+
+
+ 
+            }
+            
+
+
+
         }
     }
 }
--- Algorithms/Stack_FixedSize.cs
+++ Algorithms/Stack_FixedSize.cs
@@ -6,34 +6,43 @@ namespace Algorithms
 {
     class Stack_FixedSize
     {
-        public string[] Strings { get; set; }
-        public int N { get; set; } = 0;
+        private string[] _strings;
+        private int _n  = 0;
 
         public Stack_FixedSize(int capacity)
         {
-            Strings = new string[capacity];
+            _strings = new string[capacity];
         }
 
         public void Push(string newString)
         {
-            Strings[N] = newString;
-            N++;
+            _strings[_n] = newString;
+            _n++;
         }
 
         public string Pop()
         {
-            var removedString = Strings[N - 1];
-            N--;
+            var removedString = _strings[_n - 1];
+            _n--;
             return removedString;
         }
 
         public bool IsEmpty()
         {
-            return N == 0;
+            return _n == 0;
         }
 
-        public int Size() => N;
+        public int Size() => _n;
 
 
-    }
+        public void SeeStack()
+        {
+            for (var i = _n-1; i < 0; i--)
+            {
+                Console.WriteLine(_strings[i]);
+            }
+        }   
+            
+
+}
 }
